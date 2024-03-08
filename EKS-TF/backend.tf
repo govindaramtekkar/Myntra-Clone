2558dd3eae9be21fe84f06c68a06c0c4427652c7
govindaramtekkar <103483724+govindaramtekkar@users.noreply.github.com>
2024-03-08 14:54:16 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "myntra-test" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "eu-central-1"
   }
 }
